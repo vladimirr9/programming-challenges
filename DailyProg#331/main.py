972188a70e5c9d45750506d2d76365288446b670
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-10-16 23:50:15 +0200

    solved Daily Prog #331

---
+++ DailyProg#331/main.py
@@ -0,0 +1,31 @@
+str = 'yxyxyx'
+
+
+def flip_x(val: str) -> str:
+    ind = val.find('y')
+    if ind < 0: return val
+    return val[:ind] + "x" + val[ind + 1:]
+
+
+def flip_y(val: str) -> str:
+    ind = val.rfind('x')
+    if ind < 0: return val
+    return val[:ind] + "y" + val[ind + 1:]
+
+
+def is_solved(val: str) -> bool:
+    if val is None or val == '':
+        return True
+    if val.find('y') == -1:
+        return True
+    if val.find('x') == -1:
+        return True
+    return val.rfind('x') < val.find('y')
+
+
+def get_min_changes(val: str) -> int:
+    if is_solved(val):
+        return 0
+    return 1 + min(get_min_changes(flip_x(val)), get_min_changes(flip_y(val)))
+
+print(get_min_changes(str))
