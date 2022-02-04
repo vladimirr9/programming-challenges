295b8c840f57d41f1d6ae5019b51809612480945
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2022-02-04 20:43:26 +0100

    feat: add MinAvgTwoSlice

--- Codility/MinAvgTwoSlice.py
+++ Codility/MinAvgTwoSlice.py
@@ -1,4 +1,17 @@
+import sys
 
 
 def solution(A):
-    return min(A)
+    min_value = sys.maxsize
+    min_it = -1
+    for i in range(len(A)-1):
+        value = (A[i] + A[i+1]) / 2
+        if value < min_value:
+            min_it = i
+            min_value = value
+    for i in range(len(A)-2):
+        value = (A[i] + A[i+1] + A[i+2]) / 3
+        if value < min_value:
+            min_it = i
+            min_value = value
+    return min_it
