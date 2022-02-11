778e29ee12228dd8b56ac84fcd06b25b6329bba5
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2022-02-11 19:16:18 +0100

    feat: add Triangles

--- Codility/Triangles.py
+++ Codility/Triangles.py
@@ -1,5 +1,12 @@
 def solution(A):
-    # write your code in Python 3.6
-    pass
+    if len(A) < 3:
+        return 0
+    A.sort()
+    for idx in range (len(A)-2):
+        if A[idx] >= 0 and A[idx] > A[idx + 2] - A[idx + 1]:
+            return 1
+    return 0
+
+
 
 
