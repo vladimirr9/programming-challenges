df96d6c11d148845db44aedfa86ca98dfc5aa53e
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2022-02-13 19:13:52 +0100

    feat: add Two Sum

--- LeetCode/1. Two Sum.py
+++ LeetCode/1. Two Sum.py
@@ -3,4 +3,19 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        existing_values = set()
+        existing_values = dict()
+        for idx, num in enumerate(nums):
+            if num in existing_values:
+                existing_values[num].append(idx)
+            else:
+                existing_values[num] = [idx]
+        for idx, num in enumerate(nums):
+            if (target-num) in existing_values:
+                if (target-num) == num:
+                    if len(existing_values[num]) == 2:
+                        return existing_values[num]
+                    else:
+                        continue
+                else:
+                    return [existing_values[num][0], existing_values[target-num][0]]
+        pass
