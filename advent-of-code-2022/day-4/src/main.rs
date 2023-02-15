84fe7c5bef864fdfefe929e38c6951fbdaa177fa
Vladimir <vladimirrokvic9@gmail.com>
2023-02-15 19:33:55 +0100

    day 4 solve 2nd part

--- advent-of-code-2022/day-4/src/main.rs
+++ advent-of-code-2022/day-4/src/main.rs
@@ -1,6 +1,26 @@
 use std::{fs};
 
 fn main() {
+    // first_problem();
+    second_problem();
+}
+
+
+fn second_problem() {
+    let filepath = "input.txt";
+    let binding = fs::read_to_string(filepath).expect("Should be able to read file");
+    let data = binding.trim();
+    let rows: Vec<&str> = data.split("\n").collect();
+    let mut total_sum : u32 = 0;
+    for row in rows {
+        if is_there_any_overlap_in_row(row) {
+            total_sum += 1;
+        }
+    }
+    println!("{total_sum}");
+}
+
+fn first_problem() {
     let filepath = "input.txt";
     let binding = fs::read_to_string(filepath).expect("Should be able to read file");
     let data = binding.trim();
@@ -22,4 +42,13 @@ fn is_there_full_overlap_in_row(row: &str) -> bool {
     let (x,y) = section2.split_once("-").expect("Should be split into two at -");
     let (x, y) : (u32, u32) = (x.parse().unwrap(), y.parse().unwrap());
     (a..b+1).all(|num| (x..y+1).contains(&num)) || (x..y+1).all(|num| (a..b+1).contains(&num))
+}
+
+fn is_there_any_overlap_in_row(row: &str) -> bool {
+    let (section1, section2) = row.split_once(",").expect("Should be split into two at ,");
+    let (a,b) = section1.split_once("-").expect("Should be split into two at -");
+    let (a, b) : (u32, u32) = (a.parse().unwrap(), b.parse().unwrap());
+    let (x,y) = section2.split_once("-").expect("Should be split into two at -");
+    let (x, y) : (u32, u32) = (x.parse().unwrap(), y.parse().unwrap());
+    (a..b+1).any(|num| (x..y+1).contains(&num)) || (x..y+1).any(|num| (a..b+1).contains(&num))
 }
