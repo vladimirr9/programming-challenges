12e0600f9c8cc4fa4065f1ea4c15b29bf089a72a
Vladimir <vladimirrokvic9@gmail.com>
2023-02-17 16:59:58 +0100

    day 6 second problem solved

--- advent-of-code-2022/day-6/src/main.rs
+++ advent-of-code-2022/day-6/src/main.rs
@@ -1,25 +1,40 @@
 use std::{collections::HashSet, fs};
 
 fn main() {
-    first_problem();
+    // first_problem();
+    second_problem();
 }
 
-fn first_problem() {
+fn second_problem() {
+    let n = 14;
     let filepath = "input.txt";
     let binding = fs::read_to_string(filepath).expect("Should be able to read file");
     let data = binding.trim();
     for i in 0..data.len() {
-        if are_four_different_chained(data, i) {
-            println!("{}", i + 4);
+        if are_N_different_chained(data, i, n) {
+            println!("{}", i + n);
             break;
         }
     }
 }
 
-fn are_four_different_chained(data: &str, i: usize) -> bool {
+fn first_problem() {
+    let n = 4;
+    let filepath = "input.txt";
+    let binding = fs::read_to_string(filepath).expect("Should be able to read file");
+    let data = binding.trim();
+    for i in 0..data.len() {
+        if are_N_different_chained(data, i, n) {
+            println!("{}", i + n);
+            break;
+        }
+    }
+}
+
+fn are_N_different_chained(data: &str, i: usize, n: usize) -> bool {
     let mut set: HashSet<char> = HashSet::new();
 
-    for j in 0..4 {
+    for j in 0..n {
         if !set.insert(data.chars().nth(i + j).unwrap()) {
             return false;
         }
