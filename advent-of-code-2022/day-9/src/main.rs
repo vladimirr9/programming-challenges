2c284f980705f0663ae7411921b0b97daf5398b3
Vladimir <vladimirrokvic9@gmail.com>
2023-02-20 21:25:58 +0100

    solve day 9 second problem

--- advent-of-code-2022/day-9/src/main.rs
+++ advent-of-code-2022/day-9/src/main.rs
@@ -4,6 +4,68 @@ fn main() {
     let filepath = "input.txt";
     let data = fs::read_to_string(filepath).expect("Should be able to read file");
     let data = data.trim();
+    
+    // first_problem(data);
+
+
+    second_problem(data);
+
+}
+
+fn second_problem(data: &str) {
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    let mut rope = vec![(0,0); 10];
+    visited.insert((0,0));
+    for line in data.split("\n") {
+        let (direction, steps) = line.split_once(" ").unwrap();
+        let steps: u32 = steps.parse().unwrap();
+        for _step in 0..steps {
+            let head = &mut rope[0];
+            *head = match direction {
+                "U" => (head.0, head.1 + 1),
+                "R" => (head.0 + 1, head.1),
+                "D" => (head.0, head.1 - 1),
+                _ => (head.0 - 1, head.1),
+            };
+            for i in 0..9 {
+                let leading_node = rope[i]; 
+                let following_node = &mut rope[i+1]; 
+                if is_adjacent(leading_node, *following_node) {
+                    continue;
+                }
+                if following_node.0 == leading_node.0 {
+                    if leading_node.1 > following_node.1 {
+                        following_node.1 += 1;
+                    } else {
+                        following_node.1 -= 1;
+                    }
+                } else if following_node.1 == leading_node.1 {
+                    if leading_node.0 > following_node.0 {
+                        following_node.0 += 1;
+                    } else {
+                        following_node.0 -= 1;
+                    }
+                } else {
+                    if leading_node.1 > following_node.1 {
+                        following_node.1 += 1;
+                    } else {
+                        following_node.1 -= 1;
+                    }
+                    if leading_node.0 > following_node.0 {
+                        following_node.0 += 1;
+                    } else {
+                        following_node.0 -= 1;
+                    }
+                }
+            }
+            let tail = rope[9];
+            visited.insert(tail);
+        }
+    }
+    println!("{}", visited.len())
+}
+
+fn first_problem(data: &str) {
     let mut tail = (0, 0);
     let mut head = (0, 0);
     let mut visited: HashSet<(i32, i32)> = HashSet::new();
@@ -18,7 +80,7 @@ fn main() {
                 "D" => (head.0, head.1 - 1),
                 _ => (head.0 - 1, head.1),
             };
-            if is_tail_adjacent(head, tail) {
+            if is_adjacent(head, tail) {
                 continue;
             }
             if tail.0 == head.0 {
@@ -52,10 +114,10 @@ fn main() {
     println!("{}", visited.len())
 }
 
-fn is_tail_adjacent(head: (i32, i32), tail: (i32, i32)) -> bool {
+fn is_adjacent(node_1: (i32, i32), node_2: (i32, i32)) -> bool {
     for i in -1..2 {
         for j in -1..2 {
-            if (head.0 + i, head.1 + j) == tail {
+            if (node_1.0 + i, node_1.1 + j) == node_2 {
                 return true;
             }
         }
