62e9ed3b61e9aca1f836f3faee2190f2458ba990
Vladimir <vladimirrokvic9@gmail.com>
2023-03-12 14:19:43 +0100

    day 14 solve problem 2

--- advent-of-code-2022/day-14/src/main.rs
+++ advent-of-code-2022/day-14/src/main.rs
@@ -11,6 +11,67 @@ fn main() {
     let filepath = "input.txt";
     let data = fs::read_to_string(filepath).expect("Should be able to read file");
     let data = data.trim();
+    first_problem(data);
+    second_problem(data);
+}
+
+
+
+fn second_problem(data: &str) {
+    let width = 1000;
+    let height = 300;
+    let mut slice = vec![vec!['.'; width]; height];
+    let sand = (0, 500);
+    let mut sand_count = 0;
+    slice[sand.0][sand.1] = '+';
+    let mut floor: usize = 0;
+    for path in data.split("\n") {
+        let coords: Vec<&str> = path.split("->").map(|coord| coord.trim()).collect();
+        for i in 1..coords.len() {
+            let (y1, x1) = coords[i - 1].split_once(",").unwrap();
+            let (y1, x1) = (y1.parse::<usize>().unwrap(), x1.parse::<usize>().unwrap());
+            let (y2, x2) = coords[i].split_once(",").unwrap();
+            let (y2, x2) = (y2.parse::<usize>().unwrap(), x2.parse::<usize>().unwrap());
+
+            let (x1, x2) = (min(x1, x2), max(x1, x2));
+            let (y1, y2) = (min(y1, y2), max(y1, y2));
+
+            if x2 + 2 > floor {
+                floor = x2 + 2;
+            }
+
+            for x in x1..=x2 {
+                slice[x][y1] = '#';
+            }
+            for y in y1..=y2 {
+                slice[x1][y] = '#';
+            }
+        }
+    }
+    for i in 0..width {
+        slice[floor][i] = '#';
+    }
+    'outer: while slice[sand.0][sand.1] != 'o' {
+        let mut position = (sand.0, sand.1);
+        loop {
+            let next_position = get_sand_move(&slice, position.0, position.1);
+            match next_position {
+                None => {
+                    slice[position.0][position.1] = 'o';
+                    sand_count += 1;
+                    continue 'outer;
+                }
+                Some(i) => {
+                    position = i;
+                }
+            };
+        }
+    }
+    print_part_slice(&slice, 0, 13, 483, 515);
+    println!("Sand count {}", sand_count);
+}
+
+fn first_problem(data: &str) {
     let width = 1000;
     let height = 300;
     let mut slice = vec![vec!['.'; width]; height];
