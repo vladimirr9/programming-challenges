1802862c2d0a1303f191518818cdd4b21bc2d833
Vladimir Rokvic <vladimir.rokvic@tietoevry.com>
2024-08-01 14:05:26 +0200

    solve 7_reverse_integer mathetmatically

--- LeetCode/7_reverse_integer/src/Main.kt
+++ LeetCode/7_reverse_integer/src/Main.kt
@@ -1,14 +1,31 @@
 import kotlin.math.absoluteValue
 
 fun main() {
-    val solution = Solution()
+    val solution = IntSolution()
     println(solution.reverse(123))
     println(solution.reverse(-123))
     println(solution.reverse(120))
 }
 
+class IntSolution {
+    fun reverse(x: Int): Int {
+        var num = x
+        var y = 0
+        while (num != 0) {
+            val lastDigit = num % 10
+            val newRes = y * 10 + lastDigit
+            if ((newRes - lastDigit) / 10 != y) {
+                return 0
+            }
+            y = newRes
+            num /= 10
+        }
+        return y
+    }
+}
 
-class Solution {
+
+class StringSolution {
     fun reverse(x: Int): Int {
         val negative = x < 0
         val reversed = x.absoluteValue.toString().reversed()
